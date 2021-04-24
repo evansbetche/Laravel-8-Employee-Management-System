4d63baf7e630f5d972cc36fd3cc7cf884f2e8876
evansbetche <cheruiyot.ebett@gmail.com>
2021-04-24 14:57:45 +0300

    Addd logout button in dashboard

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,10 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
  Route::group(['middleware'=>'auth'], function(){
 
+ 	Route::get('/', function () {
+    return view('welcome');
+});
+
 
 Route::resource('departments', 'DepartmentController');
 
